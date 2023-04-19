923fa6393abe79944e76c7d0675bfbd1236177d7
jpassgo <jeffpascoe9@gmail.com>
2023-04-19 13:39:52 -0500

    About half way through the tutorial.

---
+++ finest-fitness/src/App.tsx
@@ -0,0 +1,38 @@
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+
+function App() {
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        setSelectedPage={setSelectedPage}
+        selectedPage={selectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+    </div>
+  );
+}
+
+export default App;
